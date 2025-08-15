2768ec2518b2abd348d6e00ad535944f2e90d4b6
Elizabth-Zelaya-R <elizabeth.zelaya@unah.hn>
2025-08-15 12:08:23 -0600

    nuevo

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,4 +13,39 @@ public function index() {
     return view('store.index', compact('products'));
 }
 
+// Mostrar formulario para crear producto
+public function create()
+{
+    return view('products.create');
 }
+
+// Guardar producto
+public function store(Request $request)
+{
+    $request->validate([
+        'name'        => 'required|string|max:255',
+        'description' => 'nullable|string',
+        'price'       => 'required|numeric|min:0',
+        'image'       => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+    ]);
+
+    $product = new Product();
+    $product->name        = $request->name;
+    $product->description = $request->description;
+    $product->price       = $request->price;
+
+    if ($request->hasFile('image')) {
+        $imageName = time() . '.' . $request->image->extension();
+        $request->image->move(public_path('images'), $imageName);
+        $product->image = $imageName;
+    }
+
+    $product->save();
+
+    return redirect()->route('products.index')->with('success', 'Producto agregado correctamente.');
+}
+
+}
+
+ 
+
